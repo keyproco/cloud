59088ccb0f922972be6812a65a3911d7f1997f91
keyproco <keyproco.rd@gmail.com>
2023-12-04 20:40:57 +0100

    create api_lambda
    apigateway will call this lambda function

---
+++ aws/tf-serverless-app/api_lambda.tf
@@ -0,0 +1,32 @@
+data "archive_file" "api_lambda" {
+  type        = "zip"
+  source_file = "./src/api_lambda.py"
+  output_path = "./src/api_lambda.zip"
+}
+
+resource "aws_lambda_function" "api_lambda" {
+  filename      = "./src/api_lambda.zip"
+  function_name = "api_lambda"
+  role          = aws_iam_role.lambda_notification_role.arn
+  handler       = "api_lambda.lambda_handler"
+
+  source_code_hash = data.archive_file.api_lambda.output_base64sha256
+
+  runtime          = "python3.8" 
+
+  environment {
+    variables = {
+      SM_ARN = aws_sfn_state_machine.sfn_state_machine.arn
+    }
+  }
+}
+
+
+resource "aws_lambda_permission" "apigw_lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = aws_api_gateway_rest_api.api.execution_arn
+}
