ea089b170c549372dc2fa0a96eaf2c3d59576b92
keyproco <keyproco.rd@gmail.com>
2024-04-03 22:01:28 +0200

    terraform format

--- aws/tf-terransible/main.tf
+++ aws/tf-terransible/main.tf
@@ -6,8 +6,8 @@ resource "aws_vpc" "terransible_vpc" {
   cidr_block = "10.42.0.0/16"
 
   enable_dns_hostnames = true
-  enable_dns_support = true
-  tags = { 
-    Name = "terransible_vpc" 
-    }
+  enable_dns_support   = true
+  tags = {
+    Name = "terransible_vpc"
+  }
 }
