26bd93319eac89ef64437884fcae6d4202e4a986
keyproco <keyproco.rd@gmail.com>
2024-09-08 07:22:27 +0200

    pull & launch a dotnet app from github

--- azure/app/main.tf
+++ azure/app/main.tf
@@ -1,9 +1,9 @@
-resource "azurerm_service_plan" "web" {
+resource "azurerm_service_plan" "sp" {
   name                = "web-appserviceplan"
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  os_type = "Linux"
+  os_type = "Windows"
   sku_name = "S1"
 
   tags = {
@@ -12,13 +12,35 @@ resource "azurerm_service_plan" "web" {
 }
 
 
-resource "azurerm_app_service" "app" {
-  name                = "app-${var.resource_group_name}"
+resource "azurerm_windows_web_app" "w_webapp" {
+  name                = "windows-app-${var.resource_group_name}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_service_plan.web.id
+  service_plan_id = azurerm_service_plan.sp.id
 
   site_config {
-    linux_fx_version = "DOCKER|ealen/echo-server:latest"
+
   }
+
+  depends_on = [
+    azurerm_app_service_source_control_token.github
+  ]
+}
+
+
+resource "azurerm_app_service_source_control" "webapp_source_control" {
+  app_id               = azurerm_windows_web_app.w_webapp.id
+  repo_url             = "https://github.com/keyproco/ausemartweb.git"
+  branch               = "main"
+
+   depends_on = [
+    azurerm_app_service_source_control_token.github
+  ]
+
+}
+
+
+resource "azurerm_app_service_source_control_token" "github" {
+  type  = "GitHub"
+  token = var.github_token
 }
--- azure/app/variables.tf
+++ azure/app/variables.tf
@@ -26,3 +26,7 @@ variable "client_secret" {
 variable "tenant_id" {
   type = string
 }
+
+variable "github_token" {
+    type = string
+}
